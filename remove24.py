82ef746f30f6b9c085c030e729667ac3637f7bb1
angelineemi <48149076+angelineemi@users.noreply.github.com>
2019-08-10 15:21:54 +0530

    Create remove24.py

---
+++ remove24.py
@@ -0,0 +1,6 @@
+list1=[12,24,35,24,88,120,155]
+#removes first matching value
+#del removes item at specific index
+#pop removes atem at specific index and returns it
+list1.remove(24)
+print(list1)
